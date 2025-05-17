6f34561464f7290b7c40af97ed9a93d6cd1e5d5c
Michael Obi <michael.u.obi1@gmail.com>
2025-05-17 17:47:47 -0500

    made changes to filter buttons

--- src/components/projectTag.jsx
+++ src/components/projectTag.jsx
@@ -4,9 +4,9 @@ import React from "react"
 import { motion } from "framer-motion"
 
 const ProjectTag = ({name, onClick, isSelected}) => {
-    const buttonStyle = isSelected ? "bg-black text-white" : "hover:bg-gray-100"
+  const buttonStyle = isSelected ? "bg-black text-white" : "hover:bg-gray-100 transition-colors duration-200"
   return (
-    <motion.button className={`${buttonStyle} px-3 py-1 text-sm rounded-full border-2 border-black cursor-pointer lg:px-4 lg:py-2 lg:text-xl`} whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.98 }} onClick={() => onClick(name)}>
+    <motion.button className={`${buttonStyle} px-3 py-1 text-sm rounded-full border-2 border-black cursor-pointer lg:px-4 lg:py-2 lg:text-xl`} whileTap={{ scale: 0.95 }} onClick={() => onClick(name)}>
       {name}
     </motion.button>
   )
